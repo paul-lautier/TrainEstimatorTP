81df1f97f16933925c4054d782123aa03eea78b4
Paulex <paulalexis.verrier@ynov.com>
2024-05-08 22:30:19 +0200

    feat(tests): rebased added all first version tests

--- ts/src/CalculateIndividualTicketPrice.ts
+++ ts/src/CalculateIndividualTicketPrice.ts
@@ -1,53 +1,56 @@
-import {ApiException, DiscountCard, InvalidTripInputException, Passenger, TripRequest} from "./model/trip.request";
+import { ApiException, DiscountCard, InvalidTripInputException, Passenger, TripRequest } from "./model/trip.request";
 
-export class calculateIndividualTicketPrice {
+export class CalculateIndividualTicketPrice {
+	async calculateIndividualTicketPrice(
+		fetchTicketWithParams: number,
+		passenger: Passenger,
+		trainDetails: TripRequest
+	): Promise<number> {
+		const CHILD_FARE = 9;
+		const SPECIAL_DISCOUNT = 1;
+		if (passenger.discounts.includes(DiscountCard.Family)) {
+			return fetchTicketWithParams * 0.7;
+		}
+		let individualTicketPrice = fetchTicketWithParams;
 
-    async calculateIndividualTicketPrice(fetchTicketWithParams: number, passenger: Passenger, trainDetails: TripRequest): Promise<number> {
-        const CHILD_FARE = 9;
-        const SPECIAL_DISCOUNT = 1;
-        if (passenger.discounts.includes(DiscountCard.Family)) {
-            return fetchTicketWithParams * 0.7;
-        }
-        let individualTicketPrice = fetchTicketWithParams;
+		if (passenger.age < 0) {
+			throw new InvalidTripInputException("Age is invalid");
+		}
 
-        if (passenger.age < 0) {
-            throw new InvalidTripInputException("Age is invalid");
-        }
+		if (passenger.age <= 17) {
+			individualTicketPrice = fetchTicketWithParams * 0.6;
+		} else if (passenger.age >= 70) {
+			individualTicketPrice = fetchTicketWithParams * 0.8;
+			if (passenger.discounts.includes(DiscountCard.Senior)) {
+				individualTicketPrice -= fetchTicketWithParams * 0.2;
+			}
+		} else {
+			individualTicketPrice = fetchTicketWithParams * 1.2;
+		}
 
-        if (passenger.age <= 17) {
-            individualTicketPrice = fetchTicketWithParams * 0.6;
-        } else if (passenger.age >= 70) {
-            individualTicketPrice = fetchTicketWithParams * 0.8;
-            if (passenger.discounts.includes(DiscountCard.Senior)) {
-                individualTicketPrice -= fetchTicketWithParams * 0.2;
-            }
-        } else {
-            individualTicketPrice = fetchTicketWithParams * 1.2;
-        }
+		const currentDate = new Date();
+		const dateDifference = trainDetails.details.when.getTime() - currentDate.getTime();
+		const daysDifference = dateDifference / (1000 * 3600 * 24);
+		const timeDifference = trainDetails.details.when.getTime() - currentDate.getTime();
+		const hoursDifference = timeDifference / (1000 * 3600);
 
-        const currentDate = new Date();
-        const dateDifference = trainDetails.details.when.getTime() - currentDate.getTime();
-        const daysDifference = dateDifference / (1000 * 3600 * 24);
-        const timeDifference = trainDetails.details.when.getTime() - currentDate.getTime();
-        const hoursDifference = timeDifference / (1000 * 3600);
+		if (daysDifference >= 5 && daysDifference <= 30) {
+			individualTicketPrice -= (20 - daysDifference) * 0.02 * fetchTicketWithParams;
+		} else if (daysDifference > 30) {
+			individualTicketPrice -= fetchTicketWithParams * 0.8;
+		}
+		if (hoursDifference <= 6) {
+			individualTicketPrice -= fetchTicketWithParams * 0.8;
+		}
 
-        if (daysDifference >= 5 && daysDifference <= 30) {
-            individualTicketPrice -= (20 - daysDifference) * 0.02 * fetchTicketWithParams;
-        } else if (daysDifference > 30) {
-            individualTicketPrice -= fetchTicketWithParams * 0.8;
-        }
-        if (hoursDifference <= 6) {
-            individualTicketPrice -= fetchTicketWithParams * 0.8;
-        }
+		if (passenger.age > 0 && passenger.age < 4) {
+			individualTicketPrice = CHILD_FARE;
+		}
 
-        if (passenger.age > 0 && passenger.age < 4) {
-            individualTicketPrice = CHILD_FARE;
-        }
+		if (passenger.discounts.includes(DiscountCard.TrainStroke)) {
+			individualTicketPrice = SPECIAL_DISCOUNT;
+		}
 
-        if (passenger.discounts.includes(DiscountCard.TrainStroke)) {
-            individualTicketPrice = SPECIAL_DISCOUNT;
-        }
-
-        return individualTicketPrice;
-    }
+		return individualTicketPrice;
+	}
 }
