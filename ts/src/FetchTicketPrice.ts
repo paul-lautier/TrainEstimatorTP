d9315c1e5ff3af2a34a399dd537d81e06160d56f
paul-lautier <paul.lautier64@gmail.Com>
2024-05-09 15:38:33 +0200

    add comments to explain the code

--- ts/src/FetchTicketPrice.ts
+++ ts/src/FetchTicketPrice.ts
@@ -1,17 +1,22 @@
 import { ApiException, TripRequest } from "./model/trip.request";
 
 export class FetchTicketPrice {
+	// This method fetches the ticket price based on the trip details
 	async fetchTicketPrice(trainDetails: TripRequest): Promise<number> {
+		// Send a GET request to the API endpoint with the trip details as query parameters
 		const response = await fetch(
 			`https://sncftrenitaliadb.com/api/train/estimate/price?from=${trainDetails.details.from}&to=${trainDetails.details.to}&date=${trainDetails.details.when}`
 		).then((res) => res.json());
 
+		// Extract the price from the response data
 		const fetchTicketWithParams = response.data?.price || -1;
 
+		// If the price is -1, throw an ApiException
 		if (fetchTicketWithParams === -1) {
 			throw new ApiException();
 		}
 
+		// Return the fetched ticket price
 		return fetchTicketWithParams;
 	}
 }
--- ts/src/ValidateInput.ts
+++ ts/src/ValidateInput.ts
@@ -1,15 +1,19 @@
 import { InvalidTripInputException, TripRequest } from "./model/trip.request";
 
 export class ValidateInput {
+	// This method validates the input trip details
 	validateInput(trainDetails: TripRequest) {
+		// If the 'from' field in the trip details is empty, throw an InvalidTripInputException
 		if (trainDetails.details.from.trim().length === 0) {
 			throw new InvalidTripInputException("Start city is invalid");
 		}
 
+		// If the 'to' field in the trip details is empty, throw an InvalidTripInputException
 		if (trainDetails.details.to.trim().length === 0) {
 			throw new InvalidTripInputException("Destination city is invalid");
 		}
 
+		// If the 'when' field in the trip details is a date in the past, throw an InvalidTripInputException
 		if (
 			trainDetails.details.when <
 			new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0, 0)
